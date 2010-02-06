288514fd54efedc2b3430f103e64a5dcd42d49bb
marcelo <marcelo@dexter.juntadados.org>
2010-02-06 11:22:20 -0400

    Little Fixes at Build Script

--- scripts/apagar-tudo.sh
+++ scripts/apagar-tudo.sh
@@ -5,4 +5,5 @@ AGO="`pwd`"
 rm -rf $AGO/saida/log/* 2> /dev/null
 rm -rf $AGO/saida/valgrind/* 2> /dev/null
 rm -rf $AGO/saida/matrizes/* 2> /dev/null
+rm -rf $AGO/src/lib/ultima_compilacao_libs 2> /dev/null
 rm $AGO/saida/* 2> /dev/null
